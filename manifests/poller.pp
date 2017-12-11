020dbe9477bb73966bce5993a14278465f892e23
Jordi Prats <jprats@systemadmin.es>
2017-12-11 12:43:55 +0100

    poller snmpd

--- manifests/poller.pp
+++ manifests/poller.pp
@@ -2,6 +2,8 @@ class librenms::poller() inherits librenms {
   # 8===D~
   include ::librenms::code
 
+  include ::snmpd
+
   file { '/etc/cron.d/librenms':
     ensure  => 'link',
     target  => "${librenms::basedir}/librenms.nonroot.cron",
